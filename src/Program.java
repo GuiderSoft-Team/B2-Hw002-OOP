b06ecf2f4020375f6edd0e51441c2bbd606e42ab
Mustafa KÜRKÇÜ <110635477+mustafakurkcu05@users.noreply.github.com>
2022-11-09 16:49:44 +0300

    Add files via upload

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,17 @@
 public class Program {
     public static void main(String[] args) {
-        //TODO: Lütfen md file içerisinde belirtilen Ödevi uygun şekilde yerine getiriniz.
+      Student s1=new Student("Veli","Sancak",15, new int[]{78,64,84,98});
+      Student s2=new Student("Ali","Bayrak",14,new int[]{65,92,48,92});
+
+      if (s1.getGPA()> s2.getGPA()){
+          s1.showInfo();
+      }else {
+          s2.showInfo();
+      }
+
+
+
+
 
     }
 }
